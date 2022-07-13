effa22ea8083b8d7d7a2717032878e20a313a499
Daeho Lee <59327595+NizeLee@users.noreply.github.com>
2022-07-13 16:46:59 +0900

    Add files via upload

--- 01. KhuGle(Windows Api)/KhuGle/Main.cpp
+++ 01. KhuGle(Windows Api)/KhuGle/Main.cpp
@@ -5,6 +5,19 @@
 #include "KhuGleWin.h"
 #include <iostream>
 
+#pragma warning(disable:4996)
+
+#define _CRTDBG_MAP_ALLOC
+#include <cstdlib>
+#include <crtdbg.h>
+
+#ifdef _DEBUG
+#ifndef DBG_NEW
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+#define new DBG_NEW
+#endif
+#endif  // _DEBUG
+
 int main()
 {
 	CKhuGleWin *pKhuGleSample = new CKhuGleWin(640, 480);
